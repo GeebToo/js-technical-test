d6428b52fc6d313a9a9ad8174b5f9ad6052c9e67
GeebToo <jea.guibert@gmail.com>
2017-02-24 22:25:04 +0100

    feat(service): creating github provider to get issue and comments

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
 
 angular.module('angularApp', ['ui.router'])
 
-angular.module('angularApp').config(function ($stateProvider, $urlRouterProvider) {
+angular.module('angularApp').config(function ($stateProvider, $urlRouterProvider, GitHubProvider) {
   $stateProvider
     .state('home', {
       url: '/',
@@ -12,6 +12,8 @@ angular.module('angularApp').config(function ($stateProvider, $urlRouterProvider
     })
 
   $urlRouterProvider.otherwise('/')
+
+  GitHubProvider.setGitHubURL('https://api.github.com/repos/')
 })
 
 angular.module('angularApp').controller('mainController', function ($scope, $rootScope) {
@@ -19,4 +21,28 @@ angular.module('angularApp').controller('mainController', function ($scope, $roo
     'title': 'Ma super issue',
     'number': 42
   }
+})
+
+angular.module('angularApp').provider('GitHub', function () {
+  let _this = this
+  let GITHUB_URL = ''
+
+  _this.setGitHubURL = function (url) {
+    GITHUB_URL = url
+  }
+
+  _this.$get = ['$http', function ($http) {
+    return {
+      GetIssue: GetIssue,
+      GetIssueComments: GetIssueComments
+    }
+
+    function GetIssue (issueURL) {
+      return $http.get(GITHUB_URL + issueURL)
+    }
+
+    function GetIssueComments (issueURL) {
+      return $http.get(GITHUB_URL + issueURL + '/comments')
+    }
+  }]
 })
